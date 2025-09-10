0a37b94b42543b53586f6ba1e5721e9555c57705
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-10 20:55:57 +0530

    adding the function for verify otp

--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -53,3 +53,27 @@ export const sendOtp = async(name: string, email: string, template: string) => {
     await redis.set(`otp:${email}`, otp, "EX", 300);
     await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
 }
+
+export const verifyOtp = async(email: string, otp: string, next: NextFunction) => {
+    const storedOtp = await redis.get(`otp:${email}`);
+
+    if(!storedOtp){
+        return next(new ValidationError("OTP expired or not found. Please request a new one."));
+    }
+
+    const failedAttemptsKey = `otp_attempts:${email}`;
+
+    const failedAttempts = parseInt((await redis.get(failedAttemptsKey)) || "0");
+
+    if (storedOtp !== otp) {
+        if (failedAttempts >= 2) {
+            await redis.set(`otp_lock:${email}`, "locked", "EX", 1800);
+            await redis.del(`otp:${email}`, failedAttemptsKey);
+            return next(new ValidationError("Account locked due to multiple failed OTP attempts. Please try again after 30 minutes."));
+        }
+        await redis.set(failedAttemptsKey, failedAttempts + 1, "EX", 300);
+        return next(new ValidationError(`Incorrect OTP. You have ${2 - failedAttempts} attempts left.`));
+    }
+
+    await redis.del(`otp:${email}`, failedAttemptsKey);
+}
