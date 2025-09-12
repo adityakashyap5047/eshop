5d6d3511e88f5002ff5a0ff417ae893ada3698f6
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-12 21:32:05 +0530

    making the header component dynamic

--- apps/user-ui/src/shared/widgets/header/header-bottom.tsx
+++ apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -4,11 +4,13 @@ import { navItems } from "apps/user-ui/src/configs/constants";
 import { AlignLeft, ChevronDown, ChevronUp, HeartIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import useUser from 'apps/user-ui/src/hooks/useUser';
 
 const HeaderBottom = () => {
 
     const [show, setShow] = useState(false);
     const [isSticky, setIsSticky] = useState(false);
+    const { user, isLoading } = useUser();
 
     //Track scroll position
     useEffect(() => {
@@ -58,13 +60,27 @@ const HeaderBottom = () => {
                 {isSticky && (
                     <div className='flex items-center pb-2 gap-8'>
                         <div className="flex items-center gap-2">
-                            <Link href={"/login"} className='border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]'>
-                                <UserIcon />
-                            </Link>
-                            <Link href={"/login"}>
-                                <span className='block font-medium'>Hello, </span>
-                                <span className='font-semibold'>Sign In</span>
-                            </Link>
+                            {!isLoading && user ? (
+                                <>
+                                    <Link href={"/profile"} className='border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]'>
+                                        <UserIcon />
+                                    </Link>
+                                    <Link href={"/profile"}>
+                                        <span className='block font-medium'>Hello, </span>
+                                        <span className='font-semibold'>{user?.name?.split(" ")[0]}</span>
+                                    </Link>
+                                </>
+                            ) : (
+                                <>
+                                    <Link href={"/login"} className='border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]'>
+                                        <UserIcon />
+                                    </Link>
+                                    <Link href={"/login"}>
+                                        <span className='block font-medium'>Hello, </span>
+                                        <span className='font-semibold'>{isLoading ? "..." : "Sign In"}</span>
+                                    </Link>
+                                </>
+                            )}
                         </div>
                         <div className="flex items-center gap-5">
                             <Link href={"/wishlist"} className="relative">
