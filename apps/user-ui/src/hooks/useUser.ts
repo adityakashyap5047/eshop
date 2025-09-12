1f7ed462395769efbba4d83c0ddb0c14eca8053b
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-12 21:08:45 +0530

    creating the custom hook for fetching the user data

---
+++ apps/user-ui/src/hooks/useUser.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../utils/axiosInstance";
+
+//fetch user details from API
+const fetchUser = async () => {
+    const response = await axiosInstance.get("/api/logged-in-user");
+    return response.data.user;
+}
+
+const useUser = () => {
+    const {data: user, isLoading, isError, refetch} = useQuery({
+        queryKey: ["user"],
+        queryFn: fetchUser,
+        staleTime: 5 * 60 * 1000,
+        retry: 1,
+    });
+
+    return {user, isLoading, isError, refetch};
+}
+
+export default useUser;
