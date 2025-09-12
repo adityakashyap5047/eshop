aad1f62f27dc8097a202df0f8a8b08c33f62dd3b
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-12 20:47:22 +0530

    created the middleware for getting the user data

---
+++ packages/middleware/isAuthenticated.ts
@@ -0,0 +1,34 @@
+import prisma from "@packages/libs/prisma";
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = async(req: any, res: Response, next: NextFunction) => {
+    try {
+        const token = req.cookies.access_token || req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({ error: "Unauthorized! Token missing." });
+        }
+
+        // Verify token
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as { id: string; role: "user" | "seller" };
+        
+        if (!decoded) {
+            return res.status(401).json({ error: "Unauthorized! Invalid token." });
+        }
+
+        const account = await prisma.users.findUnique({ where: { id: decoded.id } });
+
+        req.user = account;
+
+        if (!account) {
+            return res.status(401).json({ error: "Unauthorized! Account not found." });
+        }
+
+        return next();
+    } catch (error) {
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export default isAuthenticated;
