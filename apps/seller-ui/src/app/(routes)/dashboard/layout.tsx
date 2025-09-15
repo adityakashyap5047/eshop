3f9ea2cb61c03eac9090b5edaef0bcd56252f846
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-15 17:22:55 +0530

    added the layout for seller

--- apps/seller-ui/src/app/(routes)/dashboard/layout.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/layout.tsx
@@ -1,7 +1,19 @@
+import Sidebar from "apps/seller-ui/src/shared/components/sidebar/sidebar"
+
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>
-        {children}
+    <div className="flex h-full bg-black min-h-screen">
+        {/* Sidebar  */}
+        <aside className="w-[280px] min-w-[250px] max-w-[300px] border-r border-r-slate-800 text-white p-4">
+            <div className="sticky top-0">
+                <Sidebar />
+            </div>
+        </aside>
+        <main className="flex-1">
+            <div className="overflow-auto">
+                {children}
+            </div>
+        </main>
     </div>
   )
 }
--- apps/seller-ui/src/app/layout.tsx
+++ apps/seller-ui/src/app/layout.tsx
@@ -1,10 +1,17 @@
 import './global.css';
 import Providers from './provider';
+import { Poppins } from 'next/font/google';
 
 export const metadata = {
   title: 'Eshop - Seller',
   description: 'Your one-stop online shopping destination for quality products at great prices. Browse thousands of items across electronics, fashion, home goods, and more with fast shipping and secure checkout.',
 }
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+})
 
 export default function RootLayout({
   children,
@@ -13,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`min-h-screen bg-slate-900 font-sans antialiased ${poppins.variable}`}>
         <Providers>
           {children}
         </Providers>
