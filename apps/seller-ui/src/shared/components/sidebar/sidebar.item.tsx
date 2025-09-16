c0afef08bccad7608994b71a3cad3362996ccc08
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-16 16:28:45 +0530

    completed the sidebar

--- apps/seller-ui/src/shared/components/sidebar/sidebar.item.tsx
+++ apps/seller-ui/src/shared/components/sidebar/sidebar.item.tsx
@@ -10,7 +10,7 @@ interface Props{
 
 const SidebarItem = ({icon, title, isActive, href}: Props) => {
   return (
-    <Link href={href} className='my-2 block'>
+    <Link href={href} className='my-1 block'>
       <div className={`flex gap-2 w-full min-h-12 h-full items-center px-[13px] rounded-lg cursor-pointer transition hover:bg-[#2b2f31] ${isActive && "scale-[.98] bg-[#0f3158] fill-blue-200 hover:bg-[#0f3158d6]"}`}>
         {icon}
         <h5 className="text-slate-200 text-lg">
