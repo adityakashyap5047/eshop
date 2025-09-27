a90df3e5798eb2114a91c3537164b923ff805b2b
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-27 13:46:46 +0530

    created the section for top shops

--- apps/user-ui/src/app/page.tsx
+++ apps/user-ui/src/app/page.tsx
@@ -6,6 +6,7 @@ import SectionTitle from '../shared/widgets/section/section-title'
 import { useQuery } from '@tanstack/react-query'
 import axiosInstance from '../utils/axiosInstance'
 import ProductCard from '../shared/components/cards/product-card';
+import ShopCard from '../shared/components/cards/shop-card';
 
 const Page = () => {
 
@@ -28,6 +29,15 @@ const Page = () => {
     },
     staleTime: 1000 * 60 * 2,
   });
+
+  const {data: shops, isLoading: isShopLoading, isError: isShopError} = useQuery({
+    queryKey: ["shops"],
+    queryFn: async() => {
+      const res = await axiosInstance.get("/product/api/top-shops");
+      return res.data.shops;
+    },
+    staleTime: 1000 * 60 * 2,
+  })
 
   return (
     <div className='bg-[#f5f5f5]'>
@@ -81,6 +91,37 @@ const Page = () => {
             ))}
           </div>
         )}
+
+        {latestProducts?.length === 0 && (
+          <p className="text-center">No Products Available yet!</p>
+        )}
+
+        <div className="mb-8">
+          <SectionTitle title='Top Shops' />
+        </div>
+
+        {!isShopLoading && (
+          <div className="m-auto py-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2">
+            {shops?.map((shop: any) => (
+              <ShopCard key={shop.id} shop={shop} />
+            ))}
+          </div>
+        )}
+        {isShopLoading && (
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-5">
+            {Array.from({length: 10}).map((_, index) => (
+              <div 
+                key={index}
+                className='h-[250px] bg-gray-300 animate-pulse rounded-xl'
+              />
+            ))}
+          </div>
+        )}
+        {isShopError || shops?.length === 0 && (
+          <p className="text-center">No Products Available yet!</p>
+        )}
+
+        
       </div>
     </div>
   )
