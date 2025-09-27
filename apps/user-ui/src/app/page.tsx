fc6ca4352c729580f6bae88cbf85a7d7b2ec8d08
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-27 14:18:22 +0530

    finlizing the home page

--- apps/user-ui/src/app/page.tsx
+++ apps/user-ui/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { use } from 'react'
 import Hero from '../shared/components/hero'
 import SectionTitle from '../shared/widgets/section/section-title'
 import { useQuery } from '@tanstack/react-query'
@@ -14,7 +14,6 @@ const Page = () => {
     queryKey: ["products"],
     queryFn: async() => {
       const res = await axiosInstance.get("/product/api/get-all-products?page=1&limit=10");
-      console.log(res.data);
       return res.data.products;
     },
     staleTime: 1000 * 60 * 2,
@@ -39,6 +38,15 @@ const Page = () => {
     staleTime: 1000 * 60 * 2,
   })
 
+  const {data: offers, isLoading: isOfferLoading, isError: isOfferError} = useQuery({
+    queryKey: ["offers"],
+    queryFn: async() => {
+      const res = await axiosInstance.get("/product/api/get-all-events?page=10&limit=10");
+      return res.data.events;
+    },
+    staleTime: 1000 * 60 * 2,
+  })
+
   return (
     <div className='bg-[#f5f5f5]'>
       <Hero />
@@ -57,7 +65,7 @@ const Page = () => {
           </div>
         )}
 
-        {!isLoading && !isError && (
+        {!isLoading && !isError && products?.length > 0 && (
           <div className="m-auto py-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2">
             {products?.map((product: any) => (
               <ProductCard key={product.id} product={product}/>
@@ -66,7 +74,7 @@ const Page = () => {
         )}
 
         {products?.length === 0 && (
-          <p className='text-center'>
+          <p className='text-center my-2 p-2 rounded-md bg-gray-300'>
             No Products available yet
           </p>
         )}
@@ -84,7 +92,7 @@ const Page = () => {
             ))}
           </div>
         )}
-        {!isLatestProductLoading && (
+        {!isLatestProductLoading && latestProducts?.length > 0 && (
           <div className="m-auto py-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2">
             {latestProducts?.map((product: any) => (
               <ProductCard key={product.id} product={product}/>
@@ -93,14 +101,14 @@ const Page = () => {
         )}
 
         {latestProducts?.length === 0 && (
-          <p className="text-center">No Products Available yet!</p>
+          <p className="text-center my-2 p-2 rounded-md bg-gray-300">No Products Available yet!</p>
         )}
 
         <div className="mb-8">
           <SectionTitle title='Top Shops' />
         </div>
 
-        {!isShopLoading && (
+        {!isShopLoading && !isShopError && shops?.length > 0 && (
           <div className="m-auto py-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2">
             {shops?.map((shop: any) => (
               <ShopCard key={shop.id} shop={shop} />
@@ -118,10 +126,33 @@ const Page = () => {
           </div>
         )}
         {isShopError || shops?.length === 0 && (
-          <p className="text-center">No Products Available yet!</p>
+          <p className="text-center my-2 p-2 rounded-md bg-gray-300">No Shop Available yet!</p>
         )}
 
-        
+        <div className="mb-8">
+          <SectionTitle title='Top Offers' />
+        </div>
+        {isOfferLoading && (
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-5">
+            {Array.from({length: 10}).map((_, index) => (
+              <div 
+                key={index}
+                className='h-[250px] bg-gray-300 animate-pulse rounded-xl'
+              />
+            ))}
+          </div>
+        )}
+        {!isOfferLoading && !isOfferError && offers?.length > 0 && (
+          <div className="m-auto py-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2">
+            {offers?.map((product: any) => (
+              <ProductCard key={product.id} product={product} isEvent={true} />
+            ))}
+          </div>
+        )}
+
+        {isOfferError || offers?.length === 0 && (
+          <p className="text-center my-2 p-2 rounded-md bg-gray-300">No Offers Available yet!</p>
+        )}
       </div>
     </div>
   )
