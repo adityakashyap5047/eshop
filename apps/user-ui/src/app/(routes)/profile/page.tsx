903eac88bd3cdf0413a153404e6f84a74c516309
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-10-02 12:05:47 +0530

    added the right navbar for user profile

--- apps/user-ui/src/app/(routes)/profile/page.tsx
+++ apps/user-ui/src/app/(routes)/profile/page.tsx
@@ -1,11 +1,38 @@
 "use client";
 
+import { useQueryClient } from "@tanstack/react-query";
 import useUser from "apps/user-ui/src/hooks/useUser";
 import StatCard from "apps/user-ui/src/shared/components/cards/stat-card";
-import { CheckCircle, Clock, Loader2, Truck } from "lucide-react";
+import axiosInstance from "apps/user-ui/src/utils/axiosInstance";
+import { Bell, CheckCircle, Clock, Inbox, Loader2, Lock, LogOut, MapPin, ShoppingBag, Truck, User } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const page = () => {
     const {user, isLoading} = useUser();
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const queryClient = useQueryClient(); 
+    
+    const queryTab = searchParams.get("active") || "Profile";
+    const [activeTab, setActiveTab] = useState(queryTab);
+
+    useEffect(() => {
+        if(activeTab !== queryTab){
+            const newParams = new URLSearchParams(searchParams);
+            newParams.set("active", activeTab);
+            router.replace(`/profile?${newParams.toString()}`); 
+        }
+    }, [activeTab]);
+
+    const logOutHandler = async() => {
+        await axiosInstance.get("/api/logout-user").then((res) => {
+            queryClient.invalidateQueries({queryKey: ["user"]});
+
+            router.push("/");
+        })
+    };
 
     return (
         <div className='bg-gray-50 p-6 pb-14'>
@@ -42,10 +69,73 @@ const page = () => {
                     />
                 </div>
                             
-                
+                <div className="mt-10 flex flex-col md:flex-row gap6">
+                    <div className="bg-white p-4 rounded-md shadow-md border border-gray-100 w-full md:w-1/5">
+                        <nav className="space-y-2">
+                            <NavItem
+                                label="Profile"
+                                Icon={User}
+                                active={activeTab === "Profile"}
+                                onClick={() => setActiveTab("Profile")}
+                            />
+                            <NavItem
+                                label="My Orders"
+                                Icon={ShoppingBag}
+                                active={activeTab === "My Orders"}
+                                onClick={() => setActiveTab("My Orders")}
+                            />
+                            <NavItem
+                                label="Inbox"
+                                Icon={Inbox}
+                                active={activeTab === "Inbox"}
+                                onClick={() => router.push("/inbox")}
+                            />
+                            <NavItem
+                                label="Notifications"
+                                Icon={Bell}
+                                active={activeTab === "Notifications"}
+                                onClick={() => setActiveTab("Notifications")}
+                            />
+                            <NavItem
+                                label="Shipping Address"
+                                Icon={MapPin}
+                                active={activeTab === "Shipping Address"}
+                                onClick={() => setActiveTab("Shipping Address")}
+                            />
+                            <NavItem
+                                label="Change Password"
+                                Icon={Lock}
+                                active={activeTab === "Change Password"}
+                                onClick={() => setActiveTab("Change Password")}
+                            />
+                            <NavItem
+                                label="Logout"
+                                Icon={LogOut}
+                                danger 
+                                onClick={() => logOutHandler()}
+                            />
+                        </nav>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
 export default page
+
+const NavItem = ({label, Icon, active, danger, onClick}: any) => (
+    <button
+        onClick={onClick}
+        className={`w-full flex items-center gap-2 px-3 py-2 rounded-md text-sm font-medium transition ${
+            active 
+                ? "bg-blue-100 text-blue-600"
+                : danger
+                ? "text-red-500 hover:bg-red-50"
+                : "text-gray-700 hover:bg-gray-100"    
+        }`}
+    >
+        <Icon className="w-4 h-4" />
+        {label}
+    </button>
+)
