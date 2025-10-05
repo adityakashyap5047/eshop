718b934ca325e5c28b77630235cc7b27aab58f61
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-10-05 16:24:58 +0530

    adding the loading and error condition on the frontend

--- apps/user-ui/src/app/(routes)/checkout/page.tsx
+++ apps/user-ui/src/app/(routes)/checkout/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { loadStripe, Appearance } from '@stripe/stripe-js'; 
 import axiosInstance from 'apps/user-ui/src/utils/axiosInstance';
+import { XCircle } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -61,6 +62,42 @@ const Page = () => {
             }
         }
     })
+
+    const appearance: Appearance = {
+        theme: "stripe"
+    };
+
+    if(loading) {
+        return (
+            <div className="flex justify-center items-center min-h-[70vh]">
+                <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-500 border-t-transparent" />
+            </div>
+        )
+    }
+
+    if(error) {
+        return (
+            <div className="flex justify-center items-center min-h-[60vh] px-4">
+                <div className="w-full text-center">
+                    <div className="flex justify-center mb-4">
+                        <XCircle className='text-red-500 w-10 h-10' />
+                    </div>
+                    <h2 className="text-xl font-semibold text-red-600 mb-2">
+                        Payment Failed 
+                    </h2>
+                    <p className="text-sm text-gray-600 mb-6">
+                        {error} <br className="hidden sm:block" />Please go back and try checking out aagain.
+                    </p>
+                    <button
+                        onClick={() => router.push("/cart")}
+                        className='bg-blue-600 text-white px-5 py-2 rounded-md hover:bg-blue-800'
+                    >
+                        Back to Cart
+                    </button>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div>
