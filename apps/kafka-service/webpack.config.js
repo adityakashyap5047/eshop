74d9f4476e9cf134e5ebdaf3f991ee7c867bb137
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-10-10 11:57:36 +0530

    fixing the alias

--- apps/kafka-service/webpack.config.js
+++ apps/kafka-service/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   resolve: {
     alias: {
       "@packages": resolve(__dirname, '../../packages/'),
+      "packages": resolve(__dirname, '../../packages'),
     },
     extensions: ['.ts', '.js'],
   },
