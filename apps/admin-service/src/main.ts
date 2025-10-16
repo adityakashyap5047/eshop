9a0efe477352a5ddec1a65846b3022c2611a2494
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-10-16 19:37:24 +0530

    created the initial server setup for admin

--- apps/admin-service/src/main.ts
+++ apps/admin-service/src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { errorMiddleware } from '@packages/error-handler/error-middleware';
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,8 @@ app.use(cookieParser());
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to admin-service!' });
 });
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 6005;
 const server = app.listen(port, () => {
